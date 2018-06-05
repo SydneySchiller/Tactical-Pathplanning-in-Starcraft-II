545918867bb5318ae819d0ad503c5c48efae7507
Bayan Mashat <b.m.mashat@gmail.com>
2018-06-05 16:55:18 -0700

    Another visualization attempt using pygame

---
+++ ProjectFiles/Visualization/viz.py
@@ -0,0 +1,5 @@
+Python 3.6.4 (v3.6.4:d48ecebad5, Dec 18 2017, 21:07:28) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
